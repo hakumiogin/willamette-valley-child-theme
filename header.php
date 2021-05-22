e9255e95b39b5772424c667ccf3009d6f4cf6f9c
Jake Frondorf <jfrondorf@maddenmedia.com>
2021-05-21 23:28:00 -0400

    header template

--- header.php
+++ header.php
@@ -22,8 +22,11 @@ use function Madden\Theme\Child\template;
 template('layout/head');
 template('layout/covid-banner', [
 	'text' => 'Covid-19 updates and travel alerts',
-	'btn_text' => 'Learn More'
+	'btn_text' => 'More Info'
 ]);
 template('layout/header', [
-	'title' => 'Flop Click!'
+	'logo_src' => get_stylesheet_directory_uri()."/public/images/logo@1.png",
+	'logo_src_2x' => get_stylesheet_directory_uri()."/public/images/logo@2.png",
+	'mag_icon' => get_stylesheet_directory_uri()."/public/images/mag_glass.svg",
+	'hero_img' => get_stylesheet_directory_uri()."/public/images/adventure_ahead.svg",
 ]);
