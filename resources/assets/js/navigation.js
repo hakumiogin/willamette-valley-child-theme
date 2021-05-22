d22ce98d9e4ce65cc26ffdbfbaecec416be1042d
Jake Frondorf <jfrondorf@maddenmedia.com>
2021-05-21 23:33:21 -0400

    added js for navigation mobile menu

---
+++ resources/assets/js/navigation.js
@@ -0,0 +1,57 @@
+import { docReady } from "./utilities"
+
+export let navigationInit = () => {
+	docReady(() => {
+		//set the main navigation links width so bolding on hover doesn't cause elements to adjust
+		els = document.querySelectorAll(".nav-bar ul.menu > li").forEach((el) => {
+			let width = el.clientWidth
+			el.style.width = (width + 9).toString() + "px"
+		});
+		// next, we recalculate widths on window resize so the text doesn't overflow
+		//Is this overkill? Yes.
+		//debouncing the event so its not called more than once every 200ms
+		
+		// window.addEventListener('resize', debounce(() => {
+		// 	els = document.querySelectorAll(".nav-bar ul.menu > li").forEach((el) => {
+		// 		console.log(el.clientHeight)
+		// 		if (el.offsetHeight > 50){ //50 is the height when it takes up 2 lines
+		// 			el.style.width = "auto";
+		// 		}
+		// 	});
+		// }, 200, false), false)
+
+		//search toggle
+		document.getElementById("searchToggle").onclick = (e) => {
+			e.preventDefault()
+			let el = document.getElementById("searchInput")
+			if (el.value === "") {
+				el.classList.toggle("toggle-search")
+				setTimeout(() => {
+					let searchBar = document.getElementById("searchInput")
+					searchBar.focus()
+				}, 600)
+			} else {
+				document.getElementById("searchForm").submit()
+			}
+		}
+
+		//mobile hambuger menu prevent background scrolling
+		document.querySelector("#menuToggle input[type='checkbox']").onclick = () => {
+			document.getElementById("willamette-body").classList.toggle("no-scroll")
+		}
+		//mobile hamburger menu +/- toggle dropdowns
+		let els = document.querySelectorAll(".menu > li > a")
+		els.forEach((el) => {
+			el.onclick = (e) => {
+				e.preventDefault()
+				el.classList.toggle("expand")
+				for (let i = 0; i < el.children.length; i++) {
+					if (el.children[i].classList.contains("toggle-icon")){
+						el.children[i].classList.toggle("toggle-icon__plus")
+					}
+				}
+			}
+		})
+
+	})
+}
---
+++ resources/assets/js/utilities.js
@@ -0,0 +1,22 @@
+export const docReady = (fn) => {
+	if (document.readyState === "complete" || document.readyState === "interactive") {
+		setTimeout(fn, 1);
+	} else {
+		document.addEventListener("DOMContentLoaded", fn);
+	}
+}
+
+export const debounce = function(func, wait, immediate){
+	var timeout;
+	return () => {
+		const context = this, args = arguments;
+		const later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		const callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
