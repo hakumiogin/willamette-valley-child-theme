49f1e18af016e55e2cf24b746a26cbc2704b8bc9
Jake Frondorf <jfrondorf@maddenmedia.com>
2021-06-01 06:11:41 -0400

    Start of image-box native static block

---
+++ resources/assets/js/gutenberg/image-box/index.js
@@ -0,0 +1,28 @@
+import { registerBlockType } from "@wordpress/blocks"
+import { __ } from "@wordpress/i18n"
+import "./style.editor.scss"
+import { Icon } from '@wordpress/components';
+import edit from './edit'
+
+const attributes = {
+	color: {
+		type: "string"
+	}
+}
+
+registerBlockType('willamette/image-box', {
+	title: __('Madden Image Box', 'mm-willametteValley-child-theme'),
+	description: __('Madden block for showing images and content beneath them', 'mm-willametteValley-child-theme'),
+	icon: <Icon icon="screenoptions" />,
+	category: "madden",
+	keywords: __("madden", 'mm-willametteValley-child-theme'),
+
+	attributes,
+
+	save: () => {
+		return null
+	},
+
+	edit: edit
+
+})
