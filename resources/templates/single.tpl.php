4bad36207e7486fde2e607d1d19c1b9accf732a4
Jake Frondorf <jfrondorf@maddenmedia.com>
2021-06-03 16:22:19 -0400

    purging comments! away!

--- resources/templates/single.tpl.php
+++ resources/templates/single.tpl.php
@@ -17,16 +17,12 @@ get_header();
              */
             the_content();
   
-            // If comments are open or we have at least one comment, load up the comment template.
-            if ( comments_open() || get_comments_number() ) :
-                comments_template();
-            endif;  
         // End the loop.
         endwhile;
         ?>
   
         </main><!-- .site-main -->
-    </div><!-- .content-area -->
+    </div><!-- .content-area end of single.tpl -->
   
 <?php get_footer(); ?>
 
