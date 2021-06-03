39d7979834a89300ade3dd0a8849eb49470ecbf4
Jake Frondorf <jfrondorf@maddenmedia.com>
2021-06-03 12:06:24 -0400

    request a visitor guide added to footer

--- resources/templates/layout/footer.tpl.php
+++ resources/templates/layout/footer.tpl.php
@@ -1,3 +1,9 @@
+<?php 
+$visitor_guide_reuseable_block = get_post( 135448 );
+$block_content = $visitor_guide_reuseable_block->post_content;
+echo $block_content;
+?>
+
 </main>
 <footer class="footer">
 
