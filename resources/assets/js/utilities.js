eeabbbb2d79b473391bd46704d6e536674e08e77
Jake Frondorf <jfrondorf@maddenmedia.com>
2021-06-03 00:36:23 -0400

    added elementinviewport utility function

--- resources/assets/js/utilities.js
+++ resources/assets/js/utilities.js
@@ -23,15 +23,33 @@ export const debounce = function(func, wait, immediate){
 
 export const getWidth = () => {
 	if (self.innerWidth) {
-	  return self.innerWidth;
+		return self.innerWidth;
 	}
   
 	if (document.documentElement && document.documentElement.clientWidth) {
-	  return document.documentElement.clientWidth;
+		return document.documentElement.clientWidth;
 	}
   
 	if (document.body) {
-	  return document.body.clientWidth;
+		return document.body.clientWidth;
 	}
 }
-  
+export const elementInViewport = (el) => {
+	var top = el.offsetTop;
+	var left = el.offsetLeft;
+	var width = el.offsetWidth;
+	var height = el.offsetHeight;
+
+	while(el.offsetParent) {
+		el = el.offsetParent;
+		top += el.offsetTop;
+		left += el.offsetLeft;
+	}
+
+	return (
+		top >= window.pageYOffset &&
+		left >= window.pageXOffset &&
+		(top + height) <= (window.pageYOffset + window.innerHeight) &&
+		(left + width) <= (window.pageXOffset + window.innerWidth)
+	);
+}
