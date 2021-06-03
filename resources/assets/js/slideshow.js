375395b3a97a0912b24557a9d501d495df669675
Jake Frondorf <jfrondorf@maddenmedia.com>
2021-06-03 00:36:40 -0400

    added scrolling functionality to slider

---
+++ resources/assets/js/slideshow.js
@@ -0,0 +1,31 @@
+import { getWidth } from "./utilities"
+
+export const slideShow = () => {
+	let leftButton = document.getElementById("slideshowleft")
+	let rightButton = document.getElementById("slideshowright")
+	let slides = document.querySelector('.category-slider')
+	let vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+	let scrollDistance = Math.min(vw, 1000)
+	let distanceScrolled = 0
+	let scrollOffset = 250
+
+	rightButton.onclick = (e) => {
+		e.preventDefault()
+		slides.style.overflow = "visible"
+		if (distanceScrolled < 1750 ){
+			distanceScrolled += Math.max(Math.floor(scrollDistance/scrollOffset) * scrollOffset, scrollOffset);
+			slides.style.transform = "translateX(-" + (distanceScrolled).toString() + "px)"
+		}
+	}
+	leftButton.onclick = (e) => {
+		e.preventDefault()
+		if (distanceScrolled > 0){
+			distanceScrolled = Math.max(distanceScrolled - scrollDistance, 0)
+			if (distanceScrolled == 0){
+				slides.style.transform = "translateX( 0px)"
+			} else {
+				slides.style.transform = "translateX( -" + (distanceScrolled).toString() + "px)"
+			}
+		}
+	}
+}
--- resources/templates/blocks/category-slider.tpl.php
+++ resources/templates/blocks/category-slider.tpl.php
@@ -1,25 +1,47 @@
-<div class="category-slider">
-<?php 
-$args = array(
-    'cat' => get_field("category")[0],
-    'posts_per_page' => 4,
-);
+<div class="category-slider-parent">
+    <div class="slider-left-button-parent">
+        <a href="#" class="slider-left-button" id="slideshowleft">
+            <svg width="18" height="44" viewBox="0 0 18 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M-1.09278e-06 22L18 43.6506L18 0.349366L-1.09278e-06 22Z" fill="#B4BC33"/>
+            </svg>
+        </a>
+    </div>
+    <div class="slider-right-button-parent">
+        <a href="#" class="slider-right-button" id="slideshowright">
+            <svg width="18" height="47" viewBox="0 0 18 47" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M18 23.5L-2.54165e-07 46.4497L1.75216e-06 0.550327L18 23.5Z" fill="#B4BC33"/>
+            </svg>
+        </a>
+    </div>
 
-    $the_query = new WP_Query( $args );
-    if ( $the_query->have_posts() ) {
-        while ( $the_query->have_posts() ) {
-            $the_query->the_post();
-			echo '<div class="category-slider__item">';
-			echo '<div class="category-slider__item__image">';
-			the_post_thumbnail();
-			echo '</div>';
-			echo '<div class="category-slider__item__title">';
-			echo '<a href="'.get_the_permalink().'">'.get_the_title().'</a>';
-			echo '</div></div>';
+    <div class="category-slider">
+
+    <?php 
+    $args = array(
+        'cat' => get_field("category")[0],
+        'posts_per_page' => 8,
+    );
+
+        $the_query = new WP_Query( $args );
+        if ( $the_query->have_posts() ) {
+            while ( $the_query->have_posts() ) {
+                $the_query->the_post();
+                echo '<div class="category-slider__item">';
+                echo '<div class="category-slider__item__image">';
+                if (get_the_post_thumbnail()){
+                    the_post_thumbnail();
+                } else {
+                    echo "<img src='http://localhost/wp-content/uploads/2021/06/Rectangle-92-229x205.png'>";
+                }
+                echo '</div>';
+                echo '<div class="category-slider__item__title">';
+                echo '<a href="'.get_the_permalink().'">'.get_the_title().'</a>';
+                echo '</div></div>';
+            }
+        } else {
+            echo "No posts";
         }
-    } else {
-		echo "No posts";
-    }
-    wp_reset_postdata();
-?>
+        wp_reset_postdata();
+    ?>
+    </div>
 </div>
