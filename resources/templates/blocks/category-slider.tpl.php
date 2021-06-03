fc2b8d0d96fa5c8ee25599286343ef8addf052a6
Jake Frondorf <jfrondorf@maddenmedia.com>
2021-06-03 14:58:32 -0400

    don't show slider when its empty now

--- resources/templates/blocks/category-slider.tpl.php
+++ resources/templates/blocks/category-slider.tpl.php
@@ -20,28 +20,37 @@
     $args = array(
         'cat' => get_field("category")[0],
         'posts_per_page' => 8,
+        $args = array(
+            'post_type'  => 'post',
+            'posts_per_page' => 6,
+            'meta_query' => array(
+                array(
+                'key' => '_thumbnail_id',
+                'compare' => 'EXISTS'
+                ),
+            )
+        )
     );
-
-        $the_query = new WP_Query( $args );
-        if ( $the_query->have_posts() ) {
-            while ( $the_query->have_posts() ) {
-                $the_query->the_post();
-                echo '<div class="category-slider__item">';
-                echo '<div class="category-slider__item__image">';
-                if (get_the_post_thumbnail()){
-                    the_post_thumbnail();
-                } else {
-                    echo "<img src='http://localhost/wp-content/uploads/2021/06/Rectangle-92-229x205.png'>";
-                }
-                echo '</div>';
-                echo '<div class="category-slider__item__title">';
-                echo '<a href="'.get_the_permalink().'">'.get_the_title().'</a>';
-                echo '</div></div>';
+    $the_query = new WP_Query( $args );
+    if ( $the_query->have_posts() && $the_query->post_count >= 4) {
+        while ( $the_query->have_posts() ) {
+            $the_query->the_post();
+            echo '<div class="category-slider__item">';
+            echo '<div class="category-slider__item__image">';
+            if (get_the_post_thumbnail()){
+                the_post_thumbnail();
+            } else {
+                echo "<img src='http://localhost/wp-content/uploads/2021/06/Rectangle-92-229x205.png'>";
             }
-        } else {
-            echo "No posts";
+            echo '</div>';
+            echo '<div class="category-slider__item__title">';
+            echo '<a href="'.get_the_permalink().'">'.get_the_title().'</a>';
+            echo '</div></div>';
         }
-        wp_reset_postdata();
+    } else {
+        echo "No posts";
+    }
+    wp_reset_postdata();
     ?>
     </div>
 </div>
