cfcc7a02e318d9126e45cb36a5d97c717ef71e0e
Jake Frondorf <jfrondorf@maddenmedia.com>
2021-06-04 02:37:26 -0400

    added footer

--- resources/templates/layout/footer.tpl.php
+++ resources/templates/layout/footer.tpl.php
@@ -1,11 +1,55 @@
-<?php 
-$visitor_guide_reuseable_block = get_post( 135448 );
-$block_content = $visitor_guide_reuseable_block->post_content;
-echo $block_content;
-?>
-
+<div class="vistor-guide-reuseable-block">
+	<?php 
+	if (!is_front_page( )){
+		$visitor_guide_reuseable_block = get_post( 135448 );
+		$block_content = $visitor_guide_reuseable_block->post_content;
+		echo $block_content;	
+	}
+	?>
+</div>
+    </div><!-- .content-area end of single.tpl -->
 </main>
 <footer class="footer">
-
+	<div class="footer__container">
+		<div class="first-footer-column">
+			<img src="<?= get_stylesheet_directory_uri();?>/resources/assets/images/whitelogo@1x.png">
+		</div>
+		<div class="second-footer-column">
+			<h4>About Us</h4>
+			<?php wp_nav_menu( array( 
+				'theme_location' => 'abbout-us-footer-menu',
+			) ); ?>
+		</div>
+		<div class="third-footer-column">
+			<h4>Willamette Valley Regions</h4>
+			<?php wp_nav_menu( array( 
+				'theme_location' => 'regions-footer-menu',
+				'container_class' => 'second-footer-columm',
+				'link_after' => '<div class="toggle-icon toggle-icon__plus"></div>'
+			) ); ?>
+		</div>
+		<div class="fourth-footer-column">
+			<h4>Explore</h4>
+			<?php wp_nav_menu( array( 
+				'theme_location' => 'explore-footer-menu',
+				'container_class' => 'third-footer-column',
+				'link_after' => '<div class="toggle-icon toggle-icon__plus"></div>'
+			) ); ?>
+		</div>
+		<div class="fifth-footer-column">
+			<ul>
+				<li><a href="#">Site Map</a></li>
+				<li><a href="#">Privacy Policy</a></li>
+			</ul>
+			<input type="text" name="email" placeholder="Email Sign Up">
+			<div class="social-icons">
+				<a href="#" aria-label="facebook"><img src="<?= get_stylesheet_directory_uri();?>/resources/assets/images/facebook.png"></a>
+				<a href="#" aria-label="instagram"><img src="<?= get_stylesheet_directory_uri();?>/resources/assets/images/instagram.png"></a>
+				<a href="#" aria-label="youtube"><img src="<?= get_stylesheet_directory_uri();?>/resources/assets/images/youtube.png"></a>
+				<a href="#" aria-label="twitter"><img src="<?= get_stylesheet_directory_uri();?>/resources/assets/images/twitter.png"></a>
+				<a href="#" aria-label="pinterest"><img src="<?= get_stylesheet_directory_uri();?>/resources/assets/images/pinterest.png"></a>
+			</div>
+		</div>
+	</div>
 </footer>
 <?php wp_footer(); ?>
