a35e6d424829aafefa2ce905e78de9ec6c9e7531
Jake Frondorf <jfrondorf@maddenmedia.com>
2021-06-04 02:37:26 -0400

    added footer

--- child/Setup/registrations.php
+++ child/Setup/registrations.php
@@ -2,14 +2,30 @@
 namespace Madden\Theme\Setup;
 use function Madden\Theme\App\config;
 
-function register_my_menus() {
+function register_willamette_menus() {
 	register_nav_menus(
 		array(
 			'header-menu' => __( 'Header Menu', config('textdomain') ),
 		)
 	);
+	register_nav_menus(
+		array(
+			'about-us-footer-menu' => __( 'About Us Footer Menu', config('textdomain') ),
+		)
+	);
+	register_nav_menus(
+		array(
+			'regions-footer-menu' => __( 'Regions Footer Menu', config('textdomain') ),
+		)
+	);
+	register_nav_menus(
+		array(
+			'explore-footer-menu' => __( 'Explore Footer Menu', config('textdomain') ),
+		)
+	);
+
 }
-add_action( 'init', __NAMESPACE__.'\register_my_menus' );
+add_action( 'init', __NAMESPACE__.'\register_willamette_menus' );
 
 function register_madden_block_pattern_category(){
 	register_block_pattern_category(
