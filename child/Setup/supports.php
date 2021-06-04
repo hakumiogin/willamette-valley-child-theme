573f2cf0abcecd49b41c64a45c60f33863dbfe3e
Jake Frondorf <jfrondorf@maddenmedia.com>
2021-06-04 02:39:10 -0400

    removing core block patterns should be done, but it crashes the editor right now?

--- child/Setup/supports.php
+++ child/Setup/supports.php
@@ -38,6 +38,7 @@ function setup_supports() {
     add_theme_support( 'align-wide' );
     add_theme_support( 'core-block-patterns' );
     add_theme_support( 'responsive-embeds' );
+    //remove_theme_support('core-block-patterns');
     add_theme_support( 'editor-color-palette', array(
         array(
             'name' => esc_attr__( 'peach', config('textdomain') ),
