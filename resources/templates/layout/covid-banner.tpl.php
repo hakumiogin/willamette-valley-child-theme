5c27d563126eadc310a5abc2364e09d9f732cb9c
Curtis Thompson <cthompson@maddenmedia.com>
2021-06-05 12:10:34 -0700

    styling lowercase for hero h1 and adjusting styles for footer request guide block

--- resources/templates/layout/covid-banner.tpl.php
+++ resources/templates/layout/covid-banner.tpl.php
@@ -1,6 +1,6 @@
 <div class="covid-banner">
 	<p class="covid-banner__text"><?= $text ?></p>
-	<a href="/covid" class="btn btn--teal covid-banner__btn">
+	<a href="/covid-19-and-travel-alerts" class="btn btn--teal covid-banner__btn">
 		<?= $btn_text ?>
 	</a>
 </div>
