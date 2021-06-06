61d9e9898976bb51b064b4fcc31dc4756762c140
Jake Frondorf <jfrondorf@maddenmedia.com>
2021-06-06 12:18:49 -0400

    made isElementInViewport function calculate if any part of the element is visible

--- resources/assets/js/utilities.js
+++ resources/assets/js/utilities.js
@@ -39,17 +39,17 @@ export const isElementInViewport = (el) => {
 	var left = el.offsetLeft;
 	var width = el.offsetWidth;
 	var height = el.offsetHeight;
-
+	
 	while(el.offsetParent) {
 		el = el.offsetParent;
 		top += el.offsetTop;
 		left += el.offsetLeft;
 	}
-
+	
 	return (
-		top >= window.pageYOffset &&
-		left >= window.pageXOffset &&
-		(top + height) <= (window.pageYOffset + window.innerHeight) &&
-		(left + width) <= (window.pageXOffset + window.innerWidth)
+		top < (window.pageYOffset + window.innerHeight) &&
+		left < (window.pageXOffset + window.innerWidth) &&
+		(top + height) > window.pageYOffset &&
+		(left + width) > window.pageXOffset
 	);
 }
