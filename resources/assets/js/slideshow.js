9d7992b52634ea3af56a547623c1ca2db7bf8080
Jake Frondorf <jfrondorf@maddenmedia.com>
2021-06-06 00:16:57 -0400

    make sure this code doesn't run when we're not using a slideshow

--- resources/assets/js/slideshow.js
+++ resources/assets/js/slideshow.js
@@ -1,38 +1,40 @@
 export const slideShow = () => {
 	let leftButton = document.getElementById("slideshowleft")
 	let rightButton = document.getElementById("slideshowright")
-	let leftSVG = leftButton.querySelector("svg")
-	let rightSVG = rightButton.querySelector("svg")
-	let slides = document.querySelector('.category-slider')
-	let vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
-	let scrollDistance = Math.min(vw, 1000)
-	let distanceScrolled = 0
-	let scrollOffset = 250
+	if (rightButton && leftButton){ //if there is a slideshow on this page
+		let leftSVG = leftButton.querySelector("svg")
+		let rightSVG = rightButton.querySelector("svg")
+		let slides = document.querySelector('.category-slider')
+		let vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+		let scrollDistance = Math.min(vw, 1000)
+		let distanceScrolled = 0
+		let scrollOffset = 250
 
-	// turn off the left control for starters
-	leftSVG.style.visibility = "hidden"
+		// turn off the left control for starters
+		leftSVG.style.visibility = "hidden"
 
-	rightSVG.onclick = (e) => {
-		e.preventDefault()
-		slides.style.overflow = "visible"
-		if (distanceScrolled < slides.offsetWidth ){
-			distanceScrolled += Math.max(Math.floor(scrollDistance/scrollOffset) * scrollOffset, scrollOffset);
-			leftSVG.style.visibility = "visible"
-		} else {
-			distanceScrolled = 0;
-			leftSVG.style.visibility = "hidden"
-		}
-		slides.style.transform = "translateX(-" + (distanceScrolled).toString() + "px)"
-	}
-	leftSVG.onclick = (e) => {
-		e.preventDefault()
-		if (distanceScrolled > 0){
-			distanceScrolled = Math.max(distanceScrolled - scrollDistance, 0)
-			if (distanceScrolled == 0){
-				slides.style.transform = "translateX( 0px)"
+		rightSVG.onclick = (e) => {
+			e.preventDefault()
+			slides.style.overflow = "visible"
+			if (distanceScrolled < slides.offsetWidth ){
+				distanceScrolled += Math.max(Math.floor(scrollDistance/scrollOffset) * scrollOffset, scrollOffset);
 				leftSVG.style.visibility = "visible"
 			} else {
-				slides.style.transform = "translateX( -" + (distanceScrolled).toString() + "px)"
+				distanceScrolled = 0;
+				leftSVG.style.visibility = "hidden"
+			}
+			slides.style.transform = "translateX(-" + (distanceScrolled).toString() + "px)"
+		}
+		leftSVG.onclick = (e) => {
+			e.preventDefault()
+			if (distanceScrolled > 0){
+				distanceScrolled = Math.max(distanceScrolled - scrollDistance, 0)
+				if (distanceScrolled == 0){
+					slides.style.transform = "translateX( 0px)"
+					leftSVG.style.visibility = "visible"
+				} else {
+					slides.style.transform = "translateX( -" + (distanceScrolled).toString() + "px)"
+				}
 			}
 		}
 	}
