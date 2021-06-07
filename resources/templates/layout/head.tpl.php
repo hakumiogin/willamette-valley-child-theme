9c2f8c2fd8fbd604dd9520578acb754ce1a7f005
Jake Frondorf <jfrondorf@maddenmedia.com>
2021-06-07 18:36:17 -0400

    made changes to prevent mobile horizontal scrolling

--- resources/templates/layout/head.tpl.php
+++ resources/templates/layout/head.tpl.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<meta name="HandheldFriendly" content="True">
 		<meta name="MobileOptimized" content="320">
+		<meta name="viewport" content="user-scalable=0;"/>
 		<link rel="icon" type="image/png" sizes="32x32" href="<?= get_stylesheet_directory_uri()?>/resources/assets/images/favicon-32x32.png">
 		<link rel="icon" type="image/svg" sizes="32x32" href="<?= get_stylesheet_directory_uri()?>/resources/assets/images/favicon.svg">
 		<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
