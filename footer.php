c6f42be89c3582bd0d458ed29a20495acf8c4d00
Jake Frondorf <jfrondorf@maddenmedia.com>
2021-06-07 18:29:07 -0400

    css tweaks for margins and removed the double footers

---
+++ footer.php
@@ -0,0 +1,16 @@
+<?php
+namespace Madden\Theme\Header;
+
+/*
+|------------------------------------------------------------------
+| Header Controller
+|------------------------------------------------------------------
+|
+| Controller for outputting layout's opening markup. Template
+| rendered here should include `wp_head()` function call.
+|
+*/
+
+use function Madden\Theme\Child\template;
+
+template("layout/footer");
