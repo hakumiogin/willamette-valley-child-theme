59493c9b024bd3525144979882390706b376c0a0
Jake Frondorf <jfrondorf@maddenmedia.com>
2021-06-09 12:13:54 -0400

    pruning the edit function for features I changed

--- resources/assets/js/gutenberg/image-box/edit.js
+++ resources/assets/js/gutenberg/image-box/edit.js
@@ -36,13 +36,10 @@ class ImageBoxEdit extends Component {
             });
         }
     }
-    componentDidUpdate(prevProps) {
-        if (prevProps.isSelected && !this.props.isSelected) {
-            this.setState({
-                selectedLink: null
-            });
-        }
-    }
+    onUploadError = message => {
+        const { noticeOperations } = this.props;
+        noticeOperations.createErrorNotice(message);
+    };
     onSelectImage = ({ id, url, alt }) => {
         this.props.setAttributes({
             id,
@@ -57,10 +54,6 @@ class ImageBoxEdit extends Component {
             alt: ""
         });
     };
-    onUploadError = message => {
-        const { noticeOperations } = this.props;
-        noticeOperations.createErrorNotice(message);
-    };
     removeImage = () => {
         this.props.setAttributes({
             id: null,
@@ -68,83 +61,13 @@ class ImageBoxEdit extends Component {
             alt: ""
         });
     };
-    updateAlt = alt => {
-        this.props.setAttributes({
-            alt
-        });
-    };
-    onImageSizeChange = url => {
-        this.props.setAttributes({
-            url
-        });
-    };
-    addNewLink = () => {
-        const { setAttributes, attributes } = this.props;
-        const { social } = attributes;
-        setAttributes({
-            social: [...social, { icon: "wordpress", link: "" }]
-        });
-        this.setState({
-            selectedLink: social.length
-        });
-    };
-    updateSocialItem = (type, value) => {
-        const { setAttributes, attributes } = this.props;
-        const { social } = attributes;
-        const { selectedLink } = this.state;
-        let new_social = [...social];
-        new_social[selectedLink][type] = value;
-        setAttributes({ social: new_social });
-    };
-    removeLink = e => {
-        e.preventDefault();
-        const { setAttributes, attributes } = this.props;
-        const { social } = attributes;
-        const { selectedLink } = this.state;
-        setAttributes({
-            social: [
-                ...social.slice(0, selectedLink),
-                ...social.slice(selectedLink + 1)
-            ]
-        });
-        this.setState({
-            selectedLink: null
-        });
-    };
-    onSortEnd = (oldIndex, newIndex) => {
-        const { setAttributes, attributes } = this.props;
-        const { social } = attributes;
-        let new_social = arrayMove(social, oldIndex, newIndex);
-        setAttributes({ social: new_social });
-        this.setState({ selectedLink: null });
-    };
-	state = {
-        selectedLink: null
-    }
     render() {
         //console.log(this.props);
-        const { className, attributes, noticeUI, isSelected } = this.props;
-        const { url, alt, id, social } = attributes;
+        const { className, attributes, noticeUI } = this.props;
+        const { url, alt, id } = attributes;
 
         return (
             <>
-                <InspectorControls>
-                    <PanelBody title={__("Image Settings", "willamette-blocks")}>
-                        {url && !isBlobURL(url) && (
-                            <TextareaControl
-                                label={__(
-                                    "Alt Text (Alternative Text)",
-                                    "willamette-blocks"
-                                )}
-                                value={alt}
-                                onChange={this.updateAlt}
-                                help={__(
-                                    "Alternative text describes your image to people can't see it. Add a short description with its key details."
-                                )}
-                            />
-                        )}
-                    </PanelBody>
-                </InspectorControls>
                 <BlockControls>
                     {url && (
                         <Toolbar>
@@ -195,34 +118,6 @@ class ImageBoxEdit extends Component {
                             allowedTypes={["image"]}
                             notices={noticeUI}
                         />
-                    )}
-                    {this.state.selectedLink !== null && (
-                        <div
-                            className={
-                                "wp-block-willamette-blocks-image-box__linkForm"
-                            }
-                        >
-                            <TextControl
-                                label={__("Icon", "willamette-blocks")}
-                                value={social[this.state.selectedLink].icon}
-                                onChange={icon =>
-                                    this.updateSocialItem("icon", icon)
-                                }
-                            />
-                            <URLInput
-                                label={__("URL", "willamette-blocks")}
-                                value={social[this.thetate.selectedLink].link}
-                                onChange={url =>
-                                    this.updateSocialItem("link", url)
-                                }
-                            />
-                            <a
-                                className="wp-block-willamette-blocks-image-box__removeLink"
-                                onClick={this.removeLink}
-                            >
-                                {__("Remove Link", "willamette-blocks")}
-                            </a>
-                        </div>
                     )}
 					<div className="wp-block-willamette-blocks-image-box__content">
 						<InnerBlocks />
