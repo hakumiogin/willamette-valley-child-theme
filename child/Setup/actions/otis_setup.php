5c9a36b2cc62263b94cfcfb41a08f39847c308f9
Jake Frondorf <jfrondorf@maddenmedia.com>
2021-06-10 14:00:13 -0400

    Otis setup

---
+++ child/Setup/actions/otis_setup.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Madden\Theme\Child\Setup;
+
+add_filter( 'wp_otis_rest_auth', function ( $params ) {
+	$params['username'] = 'API_WVVA';
+	$params['password'] = '1potat02';
+	return $params;
+});
+
+add_filter(
+	'wp_otis_listings',
+	function ( $params ) {
+		$params['type'] = 'Bed & Breakfasts|Boutique Hotels|Events|Farm & Ranch Stays|Hostels|Lakes & Reservoirs|Hotels & Motels|Resorts|Wine & Wineries';
+		$params['region'] = 'Willamette Valley';
+		return $params;
+	}
+);
