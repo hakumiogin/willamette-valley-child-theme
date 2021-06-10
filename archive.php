412d2db514067c5d7ac27d8615420b7f0a23531a
Jake Frondorf <jfrondorf@maddenmedia.com>
2021-06-10 13:58:48 -0400

    archive template

---
+++ archive.php
@@ -0,0 +1,15 @@
+<?php 
+/* Template Name: Main 
+*/ 
+
+use function Madden\Theme\Child\template;
+
+/**
+ * Renders layout's head.
+ *
+ * @see resources/templates/layout/head.tpl.php
+ */
+template('archive');
+
+
+?>
