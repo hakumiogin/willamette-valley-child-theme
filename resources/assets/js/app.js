b30ffe34602dee9053e63cc14ea80dcf4374a310
Jake Frondorf <jfrondorf@maddenmedia.com>
2021-06-12 20:24:38 -0400

    so, weird issue with anchor links leading to wrong position, I guess this is how it's fixed

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -4,10 +4,16 @@ import { navigationInit } from "./navigation"
 import { layout } from "./layout"
 import $ from "jquery"
 import { slideshow } from './slideshow'
+//import { scrollEditor } from './scrollEditor'
 
 $(document).ready(() => {
 	navigationInit()
 	//AOS.init()
 	layout()
 	slideshow()
+	if (window.location.hash.length > 0) {
+		setTimeout(function() {
+			window.scrollTo(0, $(window.location.hash).offset().top)
+		}, 100)
+	}
 })
