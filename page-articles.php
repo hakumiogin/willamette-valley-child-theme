e5066ce8df0064ca6e17449cce37aff320a611ff
Jake Frondorf <jfrondorf@maddenmedia.com>
2021-06-14 19:10:51 -0400

    fixed visual bug

---
+++ page-articles.php
@@ -0,0 +1,12 @@
+<?php 
+/*
+ Template Name: Articles
+ Template Post Type: page
+
+ */
+
+use function Madden\Theme\Child\template;
+
+template('archive');
+
+?>
--- resources/templates/archive.tpl.php
+++ resources/templates/archive.tpl.php
@@ -5,33 +5,41 @@ get_header();
 <div class="main-container">
     <div id="primary" class="content-area">
         <main id="main" class="site-main" role="main">
+        <div class="archive-page">
         <?php
         // Start the loop.
-        while ( have_posts() ) : the_post();
-            echo '<div class="category-slider__item">';
-            echo '<div class="category-slider__item__image">';
-            if (get_the_post_thumbnail("thumbnail")){
-                the_post_thumbnail();
-            } else {
-                echo "<img src='/wp-content/uploads/2021/05/requestAVisitorGuide-150x150.jpg' alt='The Willamette Valley'>";
+        $args = array(
+            'posts_per_page' => 12,
+            'post_type'  => 'post',
+        );
+        $the_query = new WP_Query( $args );
+        $colors = ["has-purple-background-color", "has-teal-background-color", "has-lime-background-color", "has-green-background-color"];
+        $i = 0;
+        if ( $the_query->have_posts()) {
+            while ( $the_query->have_posts() ) {
+                $the_query->the_post();
+                 ?>
+                <div class="archive-page__item">
+                    <a href="<?= get_the_permalink() ?>">
+                        <div class="archive-page__item__image">
+                            <?php the_post_thumbnail("big-thumbnail"); ?>
+                        </div>
+                        <div class="archive-page__item__title <?= $colors[$i] ?>">
+                            <p><?= get_the_title() ?></p>
+                        </div>
+                    </a>
+                </div>
+                <?php 
+                $i++;
+                if ($i > count($colors) - 1){
+                    $i = 0;
+                }
             }
-            echo '</div>';
-            echo '<div class="category-slider__item__title">';
-            echo '<a href="'.get_the_permalink().'">'.get_the_title().'</a>';
-            echo '</div></div>';
-
-            /*
-             * Include the post format-specific template for the content. If you want to
-             * use this in a child theme, then include a file called called content-___.php
-             * (where ___ is the post format) and that will be used instead.
-             */
-            the_content();
-        // End the loop.
-        endwhile;
-        
-        ?>
-        </main><!-- .site-main single.tpl.php -->  
-
+        }
+        wp_reset_postdata(); ?>
+        <div class="navigation"><p><?php posts_nav_link(); ?></p></div>
+    </div>
+    </main><!-- .site-main archive.tpl.php -->  
 </div>
 
 <?php 
