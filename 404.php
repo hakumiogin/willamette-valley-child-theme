8810b1c9fb2ea04fc5a1ef3a8669eb1c0cb4c24b
Curtis Thompson <cthompson@maddenmedia.com>
2021-06-15 12:10:32 -0700

    404 template

---
+++ 404.php
@@ -0,0 +1,15 @@
+<?php 
+/* Template Name: 404 
+*/ 
+
+use function Madden\Theme\Child\template;
+
+/**
+ * Renders layout's head.
+ *
+ * @see resources/templates/layout/head.tpl.php
+ */
+template('404');
+
+
+?>
