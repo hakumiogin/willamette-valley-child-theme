8458b1ff1cfde3987af98640c73f2ca810147161
Jake Frondorf <jfrondorf@maddenmedia.com>
2021-06-15 11:19:17 -0400

    Filters and search bar on articles page

--- resources/templates/archive.tpl.php
+++ resources/templates/archive.tpl.php
@@ -1,16 +1,77 @@
 <?php
+use function Madden\Theme\Child\template;
 get_header(); 
-
+$categories = ["mid-valley", "north-valley", "west-cascades", "south-valley"];
+$regions = "";
+if (isset($_GET['category'])){
+    $category = $_GET["category"];
+    if (in_array($category, $categories)) {
+        $regions = $_GET["category"];
+    }
+} else {
+    $category = "";
+}
+$show_date = false;
+if (isset($_GET["date"])){
+    $date = $_GET["date"];
+    $show_date = true;
+} else {
+    $date = "ASC";
+}
 ?>
 <div class="main-container">
     <div id="primary" class="content-area">
-        <main id="main" class="site-main" role="main">
+    <main id="main" class="site-main" role="main">
+
+
+    <div class="dropdowns">
+        <div class="dropdown">
+            <a id="dropdownlink" href="#" class="dropdown__button">Filters<span class="dropdown__button__triangle"></span></a>
+            <div id="dropdown__links" class="dropdown__content">
+                <a class="regions-toggle" href="#">Region</a>
+                <a class="date-toggle" href="#">Date</a>
+            </div>
+        </div>
+        <div class="dropdown <?= $regions ? "" : "hiddenDropdown"; ?> regionsDropdown">
+            <a id="dropdownlink" href="#" class="dropdown__button"><?= $regions ? $regions : "regions" ?><span class="dropdown__button__triangle"></span></a>
+            <div id="dropdown__links" class="dropdown__content">
+                <a class="north-valley" href="articles/?category=north-valley<?= $date ? "&date=".$date : "" ?>">North Valley</a>
+                <a class="mid-valley" href="articles/?category=mid-valley<?= $date ? "&date=".$date : "" ?>">Mid Valley</a>
+                <a class="south-valley" href="articles/?category=south-valley<?= $date ? "&date=".$date : "" ?>">South Valley</a>
+                <a class="west-cascades" href="articles/?category=west-cascades<?= $date ? "&date=".$date : "" ?>">West Cascades</a>
+            </div>
+        </div>
+        <div class="dropdown <?= $show_date ? "" : "hiddenDropdown"; ?> dateDropdown">
+            <a id="dropdownlink" href="#" class="dropdown__button"><?= isset($_GET["date"]) ? ($date == "DESC" ? "oldest" : "newest") : "date" ?><span class="dropdown__button__triangle"></span></a>
+            <div id="dropdown__links" class="dropdown__content">
+                <a class="dropdown__links__oldest" href="articles/?date=ASC<?= $category ? "&category=".$category : "" ?>">Newest</a>
+                <a class="dropdown__links__newest" href="articles/?date=DESC<?= $category ? "&category=".$category : "" ?>">Oldest</a>
+            </div>
+        </div>
+    </div> 
+    <div class="page-search">
+        <form role="search" method="get" id="searchForm" class="searchform" action="<?= home_url( '/' ) ?>" >
+            <label aria-label="submit">
+               <input type="image" src="<?= get_stylesheet_directory_uri()."/resources/assets/images/mag_glass.svg" ?>" border="0" alt="Submit" />
+            </label>
+            <label aria-label="search">
+                <input type="text" name="s" id="searchInput" placeholder="search" /></label>
+        </form>
+    </div>
         <div class="archive-page">
         <?php
-        // Start the loop.
         $args = array(
             'posts_per_page' => 12,
             'post_type'  => 'post',
+            'orderby' => 'date',
+            'order'   => $date,
+            'tax_query' => array (
+                array (
+                    'taxonomy' => 'category',
+                    'field' => 'slug',
+                    'terms' => $category,
+                ),
+            )
         );
         $the_query = new WP_Query( $args );
         $colors = ["has-purple-background-color", "has-teal-background-color", "has-lime-background-color", "has-green-background-color"];
@@ -22,7 +83,13 @@ get_header();
                 <div class="archive-page__item">
                     <a href="<?= get_the_permalink() ?>">
                         <div class="archive-page__item__image">
-                            <?php the_post_thumbnail("big-thumbnail"); ?>
+                            <?php 
+                            if (has_post_thumbnail()){
+                                the_post_thumbnail("big-thumbnail");
+                            } else {
+                                echo "<img src='/wp-content/uploads/2021/05/requestAVisitorGuide-150x150.jpg' alt='The Willamette Valley'>";
+                            }
+                            ?>
                         </div>
                         <div class="archive-page__item__title <?= $colors[$i] ?>">
                             <p><?= get_the_title() ?></p>
