8ae6a14c37d64774b9a4ee4ffd3b1512227bc5fd
Jake Frondorf <jfrondorf@maddenmedia.com>
2021-06-28 11:26:16 -0400

    added footer block html to footer.

--- resources/templates/layout/footer.tpl.php
+++ resources/templates/layout/footer.tpl.php
@@ -1,12 +1,33 @@
-<div id="vistor-guide-reuseable-block-footer" class="vistor-guide-reuseable-block">
 	<?php 
-	if (!is_front_page( )){
-		$visitor_guide_reuseable_block = get_post( 135448 );
-		$block_content = $visitor_guide_reuseable_block->post_content;
-		echo $block_content;	
-	}
+	if (!is_front_page( )):?>
+		<!-- wp:willamette-blocks/image-box {"id":140408,"url":"https://newsitedev-ama7zea-uyz4sq2cgrby4.us-3.platformsh.site/wp-content/uploads/2021/06/Rectangle-13-5-scaled.jpg","className":"is-style-full-width-center"} -->
+		<div class="wp-block-willamette-blocks-image-box is-style-full-width-center"><div style="background-image:url(https://newsitedev-ama7zea-uyz4sq2cgrby4.us-3.platformsh.site/wp-content/uploads/2021/06/Rectangle-13-5-scaled.jpg);" class="wp-block-willamette-blocks-image-box__image"><div class="wp-block-willamette-blocks-image-box__content"><!-- wp:group {"backgroundColor":"teal","textColor":"white"} -->
+		<div class="wp-block-group has-white-color has-teal-background-color has-text-color has-background"><div class="wp-block-group__inner-container"><!-- wp:columns -->
+		<div class="wp-block-columns"><!-- wp:column {"width":"20%"} -->
+		<div class="wp-block-column" style="flex-basis:20%"><!-- wp:heading -->
+		<h2>request a<br>visitor guide</h2>
+		<!-- /wp:heading --></div>
+		<!-- /wp:column -->
+		
+		<!-- wp:column {"width":"65%"} -->
+		<div class="wp-block-column" style="flex-basis:65%"><!-- wp:paragraph -->
+		<p>Your adventure begins with the official Willamette Valley Travel Guide. Request your complimentary printed guide or download a digital guide today.</p>
+		<!-- /wp:paragraph -->
+		
+		<!-- wp:buttons -->
+		<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"purple","className":"is-style-Two-Tone"} -->
+		<div class="wp-block-button is-style-Two-Tone"><a class="wp-block-button__link has-purple-background-color has-background">Get a Guide</a></div>
+		<!-- /wp:button --></div>
+		<!-- /wp:buttons --></div>
+		<!-- /wp:column --></div>
+		<!-- /wp:columns --></div></div>
+		<!-- /wp:group --></div></div></div>
+		<!-- /wp:willamette-blocks/image-box -->
+	<?php endif;
+		// $visitor_guide_reuseable_block = get_post( 135448 );
+		// $block_content = $visitor_guide_reuseable_block->post_content;
+		// echo $block_content;	
 	?>
-</div>
     </div>
 </div>
 <footer class="footer">
