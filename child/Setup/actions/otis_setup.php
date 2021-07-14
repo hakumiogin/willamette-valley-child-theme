946d21292fd672888be7f81ac5f121ccbfada1e0
Jake Frondorf <jfrondorf@maddenmedia.com>
2021-07-14 11:59:19 -0400

    removing otis filters, let's just pull everything

--- child/Setup/actions/otis_setup.php
+++ child/Setup/actions/otis_setup.php
@@ -8,11 +8,11 @@ add_filter( 'wp_otis_rest_auth', function ( $params ) {
 	return $params;
 });
 
-add_filter(
-	'wp_otis_listings',
-	function ( $params ) {
-		$params['type'] = 'Bed & Breakfasts|Boutique Hotels|Events|Farm & Ranch Stays|Hostels|Campgrounds|Glamping & Treehouses|RV Parks|Vacation Rentals|Hotels & Motels|Resorts|Wine & Wineries|Shopping|Tour Companies|Farmers Markets|Farms, Raches & U-picks|Restaurants|Breweries|Cider & Cideries|Distilleries';
-		$params['region'] = 'Willamette Valley';
-		return $params;
-	}
-);
+// add_filter(
+// 	'wp_otis_listings',
+// 	function ( $params ) {
+// 		$params['type'] = 'Bed & Breakfasts|Boutique Hotels|Events|Farm & Ranch Stays|Hostels|Campgrounds|Glamping & Treehouses|RV Parks|Vacation Rentals|Hotels & Motels|Resorts|Wine & Wineries|Shopping|Tour Companies|Farmers Markets|Farms, Raches & U-picks|Restaurants|Breweries|Cider & Cideries|Distilleries';
+// 		$params['region'] = 'Willamette Valley';
+// 		return $params;
+// 	}
+// );
