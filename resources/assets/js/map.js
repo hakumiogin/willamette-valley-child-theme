8c9a53ff9a05631c74e8c2f8588d6abfcd84c548
tripgrass <wjgrass@gmail.com>
2021-09-16 09:09:57 -0700

    map with categoriies

---
+++ resources/assets/js/map.js
@@ -0,0 +1,25 @@
+import $ from "jquery"
+export let mapJs = () => {
+	$('.filter_select').on('click', function(e){
+		$('.filter_select').removeClass('active');
+    	var $target = $(e.target),
+        	cat = $target.data('term_id');
+        	$target.addClass('active');
+        if( cat == 'all'){
+			$.each(markers,function(k,v){
+				mymap.addLayer(v);
+			})
+		}
+		else{
+			$.each(markers,function(k,v){
+				if( v.categories.includes(cat) ){
+					console.log(k,v.categories);
+					mymap.addLayer(v);
+				}
+				else{
+					mymap.removeLayer(v);				
+				}
+			})
+		}
+	})
+}
