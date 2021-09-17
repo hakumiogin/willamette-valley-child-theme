1c8a7ac2f58b0b34f46c382fbb071301f79df40c
tripgrass <wjgrass@gmail.com>
2021-09-17 08:44:50 -0700

    reginal toggle

--- resources/assets/js/map.js
+++ resources/assets/js/map.js
@@ -7,6 +7,7 @@ export let mapJs = () => {
 		$target.addClass('active');
 		var markers = window.markers,
 			mymap = window.mymap;
+
 		if( cat == 'all'){
 			$.each(markers,function(k,v){
 				mymap.addLayer(v);
@@ -24,4 +25,20 @@ export let mapJs = () => {
 			})
 		}
 	})
+	$('.region-toggle button').on('click', function(e){
+		var $target = $(e.target),
+			region = $target.attr('id');
+			var regions = {};
+			regions.south = [];
+			regions.south.center = [44.032916, -123.081473];
+			regions.south.zoom = 8;
+			regions.north = [];
+			regions.north.center = [45.104889,-123.142299];
+			regions.north.zoom = 8;
+			regions.all = [];
+			regions.all.center = [44.530919,-123.268372];
+			regions.all.zoom = 7;
+			
+		window.mymap.setView(regions[region].center, regions[region].zoom);
+	});
 }
